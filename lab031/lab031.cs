343aef2a245209156aaea61e121e57c0af080501
Egor Devyatkin <125737299+ledibonibell@users.noreply.github.com>
2023-09-23 16:28:45 +0300

    Update lab031.cs

--- lab031/lab031.cs
+++ lab031/lab031.cs
@@ -1,16 +1,34 @@
-﻿using System;
+using System;
 
 struct Vector
 {
-    public double X;
-    public double Y;
-    public double Z;
+    private double x;
+    private double y;
+    private double z;
 
     public Vector(double x = 0.0, double y = 0.0, double z = 0.0)
     {
-        X = x;
-        Y = y;
-        Z = z;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double X
+    {
+        get { return x; }
+        set { x = value; }
+    }
+
+    public double Y
+    {
+        get { return y; }
+        set { y = value; }
+    }
+
+    public double Z
+    {
+        get { return z; }
+        set { z = value; }
     }
 
     public static Vector operator +(Vector left, Vector right)
